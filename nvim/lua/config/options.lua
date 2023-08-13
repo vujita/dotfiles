15b47ee32ab0cd6ee1a5e932081784f06b3abc6f
Vu Nguyen <shardphoneix@gmail.com>
2023-08-13 12:24:51 -0700

    setup undo tree

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,11 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.swapfile = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-q>", function()
@@ -14,3 +17,11 @@ end)
 vim.keymap.set("v", "<C-q>", function()
   vim.cmd("q!")
 end)
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Undo tree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
