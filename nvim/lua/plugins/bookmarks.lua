186994216f7be89152e9b321ac829c3d6bf0b5bc
Vu Nguyen <shardphoneix@gmail.com>
2023-08-19 00:31:26 -0700

    add bookmarks plugin with which key config
    Signed-off-by: Vu Nguyen <shardphoneix@gmail.com>

---
+++ nvim/lua/plugins/bookmarks.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    -- https://github.com/MattesGroeger/vim-bookmarks
+    "MattesGroeger/vim-bookmarks",
+    keys = {
+      {
+        "mm",
+        desc = "Toggle Bookmark",
+      },
+      {
+        "mi",
+        desc = "Add/edit/remove annotation at current line",
+      },
+      {
+        "mn",
+        desc = "Jump to next bookmark in buffer",
+      },
+      {
+        "mp",
+        desc = "Jump to previous bookmark in buffer	",
+      },
+      {
+        "ma",
+        desc = "Show all bookmarks (toggle)	",
+      },
+      {
+        "mc",
+        desc = "Clear bookmarks in current buffer only",
+      },
+      {
+        "mx",
+        desc = "Clear bookmarks in all buffers	",
+      },
+    },
+  },
+}
