20c1ed4d8e6a5d798976f7260444981a028d8a6a
Vu Nguyen <shardphoneix@gmail.com>
2023-09-15 23:52:20 -0700

    add lazygit for ubuntu

--- post-install.sh
+++ post-install.sh
@@ -44,6 +44,11 @@ case $OS in
     sudo install lazygit /usr/local/bin
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
 	;;
 'FreeBSD')
 	echo "FreeBSD"
