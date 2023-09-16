8116b58d6b090bc495a79339a36ba938f2df9783
Vu Nguyen <shardphoneix@gmail.com>
2023-09-16 10:52:11 -0700

    add seperate file for codespaces setup

---
+++ codespaces-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+git config --unset commit.gpgsign
+zsh -c "curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh"
+export ZPLUG_HOME=~/.zplug
+rm -rf $ZPLUG_HOME
+git clone https://github.com/zplug/zplug $ZPLUG_HOME
+apt-get update -y && apt-get upgrade -y
+apt install -y tmux exa vim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Change shell
+chsh -s /bin/zsh
