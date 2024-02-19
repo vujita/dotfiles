f8a37f8fd92308da67e13a226869e5d206b99255
Vu Nguyen <shardphoneix@gmail.com>
2024-02-19 10:12:18 -0800

    add yaml to ensure_installed

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "javascript", "ruby" })
+      vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "javascript", "ruby", "yaml" })
     end
   end,
 }
