b19c156ce8d9f91a9bee9cebf778e80b6393e9ec
Vu Nguyen <shardphoneix@gmail.com>
2024-02-22 22:31:12 -0800

    update and add navic

--- nvim/lua/plugins/extra.lua
+++ nvim/lua/plugins/extra.lua
@@ -1,3 +1,4 @@
+-- import any extras modules here
 return {
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
@@ -7,8 +8,8 @@ return {
   { import = "lazyvim.plugins.extras.dap.nlua" },
   { import = "lazyvim.plugins.extras.test.core" },
   { import = "lazyvim.plugins.extras.vscode" },
-
-  -- import any extras modules here
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  { import = "lazyvim.plugins.extras.editor.navic" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
