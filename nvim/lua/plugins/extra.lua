332d47cc3759f7cd561885ae46673588dcbc1fd1
Vu Nguyen <shardphoneix@gmail.com>
2024-03-21 22:54:23 -0700

    remote native snippet due to tab issues, and update screen mirror mode

--- nvim/lua/plugins/extra.lua
+++ nvim/lua/plugins/extra.lua
@@ -15,5 +15,4 @@ return {
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
-  { import = "lazyvim.plugins.extras.coding.native_snippets" },
 }
