90c78af09a2b9b7a30983b9e0ee7f98feb09ab59
Vu Nguyen <shardphoneix@gmail.com>
2024-06-24 09:53:56 -0700

    update auto command

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,7 +3,7 @@
 -- Add any additional autocmds here
 
 -- Remove trailing whitespace on save
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--   pattern = "*",
+--   command = [[%s/\s\+$//e]],
+-- })
