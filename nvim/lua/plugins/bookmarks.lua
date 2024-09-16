d97b112cc5b59b2a5814bb89e516d4485c36e571
Vu Nguyen <shardphoneix@gmail.com>
2024-09-15 23:58:00 -0700

    update bookmarks keybindings

--- nvim/lua/plugins/bookmarks.lua
+++ nvim/lua/plugins/bookmarks.lua
@@ -1,34 +1,61 @@
 return {
   {
+    -- nmap <Leader>kk <Plug>BookmarkMoveUp
+    -- nmap <Leader>jj <Plug>BookmarkMoveDown
+    -- nmap <Leader>g <Plug>BookmarkMoveToLine
     -- https://github.com/MattesGroeger/vim-bookmarks
     "MattesGroeger/vim-bookmarks",
+    enabled = true,
     keys = {
       {
-        "mm",
+
+        -- nmap <Leader><Leader> <Plug>BookmarkToggle
+        "<leader>mm",
+        "<cmd>BookmarkToggle<cr>",
         desc = "Toggle Bookmark",
       },
       {
-        "mi",
+        -- nmap <Leader>i <Plug>BookmarkAnnotate
+        "<leader>mi",
+        "<cmd>BookmarkAnnotate<cr>",
         desc = "Add/edit/remove annotation at current line",
       },
       {
-        "mn",
+        -- nmap <Leader>a <Plug>BookmarkShowAll
+        "<leader>a",
+        "<cmd>BookmarkShowAll<cr>",
+        desc = "Show all bookmarks",
+      },
+      {
+
+        -- nmap <Leader>j <Plug>BookmarkNext
+        "<leader>mn",
+        "<cmd>BookmarkNext<cr>",
         desc = "Jump to next bookmark in buffer",
       },
       {
-        "mp",
+        -- nmap <Leader>k <Plug>BookmarkPrev
+        "<leader>mp",
+        "<cmd>BookmarkPrev<cr>",
         desc = "Jump to previous bookmark in buffer	",
       },
       {
-        "ma",
+        "<leader>ma",
+        "<cmd>BookmarkShowAll<cr>",
         desc = "Show all bookmarks (toggle)	",
       },
       {
-        "mc",
+
+        -- nmap <Leader>c <Plug>BookmarkClear
+        "<leader>mc",
+        "<cmd>BookmarkClear<cr>",
         desc = "Clear bookmarks in current buffer only",
       },
+
+      -- nmap <Leader>x <Plug>BookmarkClearAll
       {
-        "mx",
+        "<leader>mx",
+        "<cmd>BookmarkClearAll<cr>",
         desc = "Clear bookmarks in all buffers	",
       },
     },
