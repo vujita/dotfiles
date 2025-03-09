26e1939b7b3c1376a8cd9c2a997e86cc7f958ec5
Vu Nguyen <shardphoneix@gmail.com>
2025-03-09 10:52:01 -0700

    updates

--- brew-update-deps.sh
+++ brew-update-deps.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 
 echo "Regenerate Brewfile"
-rm Brewfile Brewfile.lock.json
 brew bundle dump
 brew bundle
 brew cleanup
--- nvim/lua/plugins/extra.lua
+++ nvim/lua/plugins/extra.lua
@@ -1,24 +1,4 @@
 -- import any extras modules here
 return {
-  { import = "lazyvim.plugins.extras.linting.eslint" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.coding.copilot" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.dap.nlua" },
-  { import = "lazyvim.plugins.extras.test.core" },
-  { import = "lazyvim.plugins.extras.vscode" },
-  { import = "lazyvim.plugins.extras.editor.aerial" },
-  { import = "lazyvim.plugins.extras.editor.navic" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lsp.none-ls" },
-  { import = "lazyvim.plugins.extras.coding.yanky" },
-  { import = "lazyvim.plugins.extras.editor.telescope" },
-  { import = "lazyvim.plugins.extras.editor.fzf" },
-  { import = "lazyvim.plugins.extras.editor.refactoring" },
-  { import = "lazyvim.plugins.extras.editor.illuminate" },
-  { import = "lazyvim.plugins.extras.coding.neogen" },
-  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+
 }
