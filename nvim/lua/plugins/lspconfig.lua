311b987611f50a7f38974a79d52ff5e3f0f799d3
Vu Nguyen <shardphoneix@gmail.com>
2026-06-13 08:11:51 -0700

    fix lazy require not using namespace

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,13 +4,18 @@ return {
     servers = { eslint = {} },
     setup = {
       eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
+        vim.api.nvim_create_autocmd("LspAttach", {
+          callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client then
+              if client.name == "eslint" then
+                client.server_capabilities.documentFormattingProvider = true
+              elseif client.name == "tsserver" then
+                client.server_capabilities.documentFormattingProvider = false
+              end
+            end
+          end,
+        })
       end,
     },
   },
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -229,56 +229,60 @@ return {
           return true
         end,
         vtsls = function(_, opts)
-          LazyVim.lsp.on_attach(function(client, buffer)
-            client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
-              ---@type string, string, lsp.Range
-              local action, uri, range = unpack(command.arguments)
+          vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if not client or client.name ~= "vtsls" then return end
+              client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+                ---@type string, string, lsp.Range
+                local action, uri, range = unpack(command.arguments)
 
-              local function move(newf)
+                local function move(newf)
+                  client.request("workspace/executeCommand", {
+                    command = command.command,
+                    arguments = { action, uri, range, newf },
+                  })
+                end
+
+                local fname = vim.uri_to_fname(uri)
                 client.request("workspace/executeCommand", {
-                  command = command.command,
-                  arguments = { action, uri, range, newf },
-                })
-              end
-
-              local fname = vim.uri_to_fname(uri)
-              client.request("workspace/executeCommand", {
-                command = "typescript.tsserverRequest",
-                arguments = {
-                  "getMoveToRefactoringFileSuggestions",
-                  {
-                    file = fname,
-                    startLine = range.start.line + 1,
-                    startOffset = range.start.character + 1,
-                    endLine = range["end"].line + 1,
-                    endOffset = range["end"].character + 1,
+                  command = "typescript.tsserverRequest",
+                  arguments = {
+                    "getMoveToRefactoringFileSuggestions",
+                    {
+                      file = fname,
+                      startLine = range.start.line + 1,
+                      startOffset = range.start.character + 1,
+                      endLine = range["end"].line + 1,
+                      endOffset = range["end"].character + 1,
+                    },
                   },
-                },
-              }, function(_, result)
-                ---@type string[]
-                local files = result.body.files
-                table.insert(files, 1, "Enter new path...")
-                vim.ui.select(files, {
-                  prompt = "Select move destination:",
-                  format_item = function(f)
-                    return vim.fn.fnamemodify(f, ":~:.")
-                  end,
-                }, function(f)
-                  if f and f:find("^Enter new path") then
-                    vim.ui.input({
-                      prompt = "Enter move destination:",
-                      default = vim.fn.fnamemodify(fname, ":h") .. "/",
-                      completion = "file",
-                    }, function(newf)
-                      return newf and move(newf)
-                    end)
-                  elseif f then
-                    move(f)
-                  end
+                }, function(_, result)
+                  ---@type string[]
+                  local files = result.body.files
+                  table.insert(files, 1, "Enter new path...")
+                  vim.ui.select(files, {
+                    prompt = "Select move destination:",
+                    format_item = function(f)
+                      return vim.fn.fnamemodify(f, ":~:.")
+                    end,
+                  }, function(f)
+                    if f and f:find("^Enter new path") then
+                      vim.ui.input({
+                        prompt = "Enter move destination:",
+                        default = vim.fn.fnamemodify(fname, ":h") .. "/",
+                        completion = "file",
+                      }, function(newf)
+                        return newf and move(newf)
+                      end)
+                    elseif f then
+                      move(f)
+                    end
+                  end)
                 end)
-              end)
-            end
-          end, "vtsls")
+              end
+            end,
+          })
           -- copy typescript settings to javascript
           opts.settings.javascript =
             vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
